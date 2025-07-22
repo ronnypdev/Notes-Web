09640f643a7a67b8d5e7f22930401bcf0689209a
Ronny Perez <ronnypdev@gmail.com>
2025-07-22 07:11:15 -0400

    Implemented basic toast styles

--- src/components/Toast/Toast.tsx
+++ src/components/Toast/Toast.tsx
@@ -1,3 +1,6 @@
+import { CheckCircleIcon, CloseIcon } from '@/components/icons';
+import { Button } from '@/components/ui/button';
+
 interface ToastProps {
   message: string;
   type:
@@ -12,5 +15,10 @@ interface ToastProps {
 }
 
 export const Toast = ({ message, type }: ToastProps) => {
-  return <div className={`toast ${type}`}>{message}</div>;
+  return (
+    <div
+      className={`toast ${type} flex items-center border border-solid border-neutral-200 rounded-lg p-[var(--spacing-100)] bg-neutral-0 shadow-toast`}>
+      {message}
+    </div>
+  );
 };
