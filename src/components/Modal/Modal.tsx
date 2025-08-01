2cf97a59caa47a4b89d43b839754e854d505fc59
Ronny Perez <ronnypdev@gmail.com>
2025-08-01 18:34:07 -0400

    fix hydration bug

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import {
   Dialog,
   DialogClose,
@@ -7,25 +8,44 @@ import {
   DialogHeader,
   DialogTitle,
   DialogTrigger,
-} from '@radix-ui/react-dialog';
-import React from 'react';
-import { Button } from '../ui/button';
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { Button } from '@/components/ui/button';
 
 export const Modal = () => {
   return (
     <Dialog>
-      <DialogTrigger>
-        <Button>Open</Button>
-      </DialogTrigger>
-      <DialogContent>
-        <DialogHeader>
-          <DialogTitle>Dialog Title</DialogTitle>
-          <DialogDescription>Dialog Description</DialogDescription>
-        </DialogHeader>
-        <DialogFooter>
-          <Button>Close</Button>
-        </DialogFooter>
-      </DialogContent>
+      <form>
+        <DialogTrigger asChild>
+          <Button variant="outline">Open Dialog</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-[425px]">
+          <DialogHeader>
+            <DialogTitle>Edit profile</DialogTitle>
+            <DialogDescription>
+              Make changes to your profile here. Click save when you&apos;re
+              done.
+            </DialogDescription>
+          </DialogHeader>
+          <div className="grid gap-4">
+            <div className="grid gap-3">
+              <Label htmlFor="name-1">Name</Label>
+              <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+            </div>
+            <div className="grid gap-3">
+              <Label htmlFor="username-1">Username</Label>
+              <Input id="username-1" name="username" defaultValue="@peduarte" />
+            </div>
+          </div>
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button variant="outline">Cancel</Button>
+            </DialogClose>
+            <Button type="submit">Save changes</Button>
+          </DialogFooter>
+        </DialogContent>
+      </form>
     </Dialog>
   );
 };
