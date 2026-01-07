5c020014109c8f3c55bbfe8242a8c284f9d6c090
Ronny Perez <ronnypdev@gmail.com>
2026-01-06 20:34:43 -0500

    implemented basic grid logic but columns are still collapsing

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -2,18 +2,16 @@ import Header from "@/components/Header/Header";
 import Main from "@/components/Main/Main";
 import LeftSideBar from "@/components/Sidebar/Left/LeftSideBar";
 import RightSideBar from "@/components/Sidebar/Right/RightSideBar";
-import Footer from "@/components/Footer/Footer";
 
 export default function Home() {
   return (
-    <>
+    <div className="grid-box">
       <Header />
       <LeftSideBar />
       <Main>
         <h1>Main</h1>
       </Main>
       <RightSideBar />
-      <Footer />
-    </>
+    </div>
   );
 }
