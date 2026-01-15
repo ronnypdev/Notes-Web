bec6cf3cecad64a212e43a274602928bc16f6e52
Ronny Perez <ronnypdev@gmail.com>
2026-01-15 12:33:56 -0500

    Removed siebars and style still broken

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,17 +1,13 @@
 import Header from "@/components/Header/Header";
 import Main from "@/components/Main/Main";
-import LeftSideBar from "@/components/Sidebar/Left/LeftSideBar";
-import RightSideBar from "@/components/Sidebar/Right/RightSideBar";
 
 export default function Home() {
   return (
     <div className="max-w-full">
       <Header />
-      <LeftSideBar />
       <Main>
         <h1>Main</h1>
       </Main>
-      <RightSideBar />
     </div>
   );
 }
