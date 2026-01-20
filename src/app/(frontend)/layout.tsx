4659a6915b68effb5753653c51e2bacefd7148ae
Ronny Perez <ronnypdev@gmail.com>
2026-01-19 21:40:53 -0500

    Implemented sidebar and header collapase

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,5 +1,7 @@
 import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
+import { SidebarProvider } from "@/components/ui/sidebar";
+import LeftSideBar from "@/components/Sidebar/Left/LeftSideBar";
 
 export default async function FrontendLayout({
   children,
@@ -9,9 +11,12 @@ export default async function FrontendLayout({
   return (
     <>
       <Header />
-      <main className="flex flex-col gap-[32px] items-center sm:items-start">
-        {children}
-      </main>
+      <SidebarProvider>
+        <LeftSideBar />
+        <main className="flex flex-col gap-[32px] items-center sm:items-start">
+          {children}
+        </main>
+      </SidebarProvider>
       <Footer />
     </>
   );
--- src/components/Sidebar/Left/LeftSideBar.tsx
+++ src/components/Sidebar/Left/LeftSideBar.tsx
@@ -1,5 +1,5 @@
-import React from "react";
+import { Sidebar } from "@/components/ui/sidebar";
 
 export default function LeftSideBar() {
-  return <aside>Left sidebar</aside>;
+  return <Sidebar side="left">Left sidebar</Sidebar>;
 }
