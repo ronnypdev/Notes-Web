33f120f82af8f4a38d00b6d1940e3690bac139cd
Ronny Perez <ronnypdev@gmail.com>
2026-01-22 10:08:42 -0500

    Implemented Sidebar header but the main layout component is breaking

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,6 +1,5 @@
-import Footer from "@/components/Footer/Footer";
 import Header from "@/components/Header/Header";
-import { SidebarProvider } from "@/components/ui/sidebar";
+import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
 import LeftSideBar from "@/components/Sidebar/Left/LeftSideBar";
 
 export default async function FrontendLayout({
@@ -10,14 +9,15 @@ export default async function FrontendLayout({
 }>) {
   return (
     <>
-      <Header />
       <SidebarProvider>
         <LeftSideBar />
+        <SidebarInset>
+          <Header />
+        </SidebarInset>
         <main className="flex flex-col gap-[32px] items-center sm:items-start">
           {children}
         </main>
       </SidebarProvider>
-      <Footer />
     </>
   );
 }
