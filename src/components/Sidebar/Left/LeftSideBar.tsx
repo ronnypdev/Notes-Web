2da7b199f9a9efc949dfca0342de8c70bd27b8e5
Ronny Perez <ronnypdev@gmail.com>
2026-01-27 17:10:45 -0500

    Implemented Allnotes and Archivenotes routes

--- src/components/Sidebar/Left/LeftSideBar.tsx
+++ src/components/Sidebar/Left/LeftSideBar.tsx
@@ -1,10 +1,14 @@
 import { Sidebar, SidebarFooter, SidebarHeader } from "@/components/ui/sidebar";
+import Link from "next/link";
 
 export default function LeftSideBar() {
   return (
     <>
       <Sidebar side="left">
-        <SidebarHeader>Left Sidebar</SidebarHeader>
+        <SidebarHeader>
+          <Link href="/allnotes">All Notes</Link>
+          <Link href="/archivenotes">Archive Notes</Link>
+        </SidebarHeader>
         <SidebarFooter>footer</SidebarFooter>
       </Sidebar>
     </>
