4fe6bfea96d33a1cb8a1a86ae11ecb2753a22a67
Ronny Perez <ronnypdev@gmail.com>
2026-01-28 10:05:20 -0500

    Sidebar Header structure implemented styles WIP

--- src/components/Sidebar/Left/LeftSideBar.tsx
+++ src/components/Sidebar/Left/LeftSideBar.tsx
@@ -8,15 +8,26 @@ import {
 } from "@/components/ui/sidebar";
 import Link from "next/link";
 import Logo from "@/components/Logo/Logo";
+import { ArchiveIcon, HomeIcon } from "@/components/icons";
 
 export default function LeftSideBar() {
   return (
     <>
       <Sidebar side="left">
         <SidebarHeader>
-          <Logo />
-          <Link href="/allnotes">All Notes</Link>
-          <Link href="/archivenotes">Archive Notes</Link>
+          <div className="py-3 px-4 my-3">
+            <Logo />
+          </div>
+          <div className="flex flex-col gap-1">
+            <div className="py-2.5 px-3 my-1 flex items-center gap-2 text-neutral-500">
+              <HomeIcon className="w-4 h-4" />
+              <Link href="/allnotes">All Notes</Link>
+            </div>
+            <div className="py-2.5 px-3 my-1 flex items-center gap-2 text-neutral-500">
+              <ArchiveIcon className="w-4 h-4" />
+              <Link href="/archivenotes">Archive Notes</Link>
+            </div>
+          </div>
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
