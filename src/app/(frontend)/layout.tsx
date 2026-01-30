e984095a837d8e4560925a7e89084c8a2e77d1aa
Ronny Perez <ronnypdev@gmail.com>
2026-01-29 19:46:20 -0500

    Layout broke againn and i have to fix it

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,7 @@
-import Header from "@/components/Header/Header";
-import { SidebarInset, SidebarProvider } from "@/components/ui/sidebar";
-import LeftSideBar from "@/components/Sidebar/Left/LeftSideBar";
-import RightSideBar from "@/components/Sidebar/Right/RightSideBar";
+import Header from '@/components/Header/Header';
+import { SidebarInset, SidebarProvider } from '@/components/ui/sidebar';
+import LeftSideBar from '@/components/Sidebar/Left/LeftSideBar';
+import RightSideBar from '@/components/Sidebar/Right/RightSideBar';
 
 export default async function FrontendLayout({
   children,
@@ -15,9 +15,7 @@ export default async function FrontendLayout({
         <SidebarInset>
           <Header />
           <div className="flex flex-1">
-            <main className="flex-1 p-4">
-              {children}
-            </main>
+            <main className="flex-1 p-4">{children}</main>
             <RightSideBar />
           </div>
         </SidebarInset>
