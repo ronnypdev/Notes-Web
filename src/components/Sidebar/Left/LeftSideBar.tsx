ceed699f7d1798da4459e4b53c83605cebfd365a
Ronny Perez <ronnypdev@gmail.com>
2026-02-04 16:42:44 -0500

    Refactor navgiation routes and completed leftside bar styles

--- src/components/Sidebar/Left/LeftSideBar.tsx
+++ src/components/Sidebar/Left/LeftSideBar.tsx
@@ -8,29 +8,11 @@ import {
   SidebarGroupLabel,
   SidebarFooter,
 } from '@/components/ui/sidebar';
-import { usePathname } from 'next/navigation';
-import Link from 'next/link';
 import Logo from '@/components/Logo/Logo';
-import { ArchiveIcon, HomeIcon, ChevronRightIcon } from '@/components/icons';
-import clsx from 'clsx';
 import NavigationList from '../NavigationList';
+import NavigationLinkRoutes from '../NavigationLinkRoutes';
 
 export default function LeftSideBar() {
-  const pathname = usePathname();
-
-  const links = [
-    {
-      href: '/allnotes',
-      icon: HomeIcon,
-      label: 'All Notes',
-    },
-    {
-      href: '/archivenotes',
-      icon: ArchiveIcon,
-      label: 'Archived Notes',
-    },
-  ];
-
   return (
     <>
       <Sidebar side="left">
@@ -38,29 +20,7 @@ export default function LeftSideBar() {
           <div className="py-3 px-4 my-3">
             <Logo />
           </div>
-          <div className="flex flex-col gap-2">
-            {links.map((link) => (
-              <Link
-                key={link.href}
-                href={link.href}
-                className={clsx(
-                  'py-1.5 px-3 flex items-center gap-2 text-neutral-950',
-                  { 'rounded-lg bg-neutral-100': pathname === link.href },
-                )}>
-                <link.icon
-                  className={clsx('w-5 h-5', {
-                    'text-blue-500': pathname === link.href,
-                  })}
-                />
-                <span>{link.label}</span>
-                <ChevronRightIcon
-                  className={clsx('w-6 h-6 ml-auto', {
-                    'text-blue-500': pathname === link.href,
-                  })}
-                />
-              </Link>
-            ))}
-          </div>
+          <NavigationLinkRoutes />
         </SidebarHeader>
         <SidebarContent>
           <SidebarGroup>
@@ -70,7 +30,9 @@ export default function LeftSideBar() {
             <NavigationList />
           </SidebarGroup>
         </SidebarContent>
-        <SidebarFooter>A place to keep all your notes organized</SidebarFooter>
+        <SidebarFooter className="text-neutral-950 bg-neutral-100 font-sans text-sm font-medium leading-[1.2] tracking-[-0.2px]">
+          A place to keep all your notes organized
+        </SidebarFooter>
       </Sidebar>
     </>
   );
---
+++ src/components/Sidebar/NavigationLinkRoutes.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link';
+import { ArchiveIcon, HomeIcon, ChevronRightIcon } from '@/components/icons';
+import { usePathname } from 'next/navigation';
+import clsx from 'clsx';
+
+export default function NavigationLinkRoutes() {
+  const pathname = usePathname();
+
+  const links = [
+    {
+      href: '/allnotes',
+      icon: HomeIcon,
+      label: 'All Notes',
+    },
+    {
+      href: '/archivenotes',
+      icon: ArchiveIcon,
+      label: 'Archived Notes',
+    },
+  ];
+
+  return (
+    <div className="flex flex-col gap-2">
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          href={link.href}
+          className={clsx(
+            'py-1.5 px-3 flex items-center gap-2 text-neutral-950',
+            { 'rounded-lg bg-neutral-100': pathname === link.href },
+          )}>
+          <link.icon
+            className={clsx('w-5 h-5', {
+              'text-blue-500': pathname === link.href,
+            })}
+          />
+          <span>{link.label}</span>
+          <ChevronRightIcon
+            className={clsx('w-6 h-6 ml-auto', {
+              'text-blue-500': pathname === link.href,
+            })}
+          />
+        </Link>
+      ))}
+    </div>
+  );
+}
