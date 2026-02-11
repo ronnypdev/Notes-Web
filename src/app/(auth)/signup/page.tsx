f75ea0c814856f094c7da41e3907d2dad7c2e2b7
Ronny Perez <ronnypdev@gmail.com>
2026-02-10 20:46:43 -0500

    Implemented signup skeleton

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,5 +1,19 @@
-import React from 'react';
+import {
+  Field,
+  FieldDescription,
+  FieldError,
+  FieldGroup,
+  FieldLabel,
+  FieldSeparator,
+  FieldSet,
+  FieldTitle,
+} from '@/components/ui/field';
+import { InputField } from '@/components/InputField/InputField';
 
 export default function SignupPage() {
-  return <div>SignupPage</div>;
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-2xl font-bold">Signup</h1>
+    </div>
+  );
 }
