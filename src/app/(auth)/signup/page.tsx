bb5cc880c328be492c4b1103f762865e9b6e6127
Ronny Perez <ronnypdev@gmail.com>
2026-02-12 19:51:03 -0500

    Installed auth depencies but work still on going

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -8,12 +8,24 @@ import {
   FieldSet,
   FieldTitle,
 } from '@/components/ui/field';
-import { InputField } from '@/components/InputField/InputField';
+import { Button } from '@/components/ui/button';
+import Logo from '@/components/Logo/Logo';
 
 export default function SignupPage() {
   return (
-    <div className="flex flex-col items-center justify-center h-screen">
-      <h1 className="text-2xl font-bold">Signup</h1>
+    <div className="flex flex-col items-center justify-center h-screen bg-neutral-100">
+      <div className="flex flex-col items-center justify-center gap-2">
+        <div className="flex flex-col items-center justify-center gap-2">
+          <Logo />
+          <h1 className="text-2xl font-bold">Welcome to Note</h1>
+          <p className="text-sm text-neutral-500">Please log in to continue</p>
+        </div>
+        <form className="flex flex-col gap-2">
+          <Button variant="default" type="submit">
+            Signup
+          </Button>
+        </form>
+      </div>
     </div>
   );
 }
