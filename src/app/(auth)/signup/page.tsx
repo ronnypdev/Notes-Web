21821683958ee8f2c274bb32b029e2692c0c4757
Ronny Perez <ronnypdev@gmail.com>
2026-02-16 20:29:53 -0500

    Inputfields are breaking WIP

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,13 +1,4 @@
-import {
-  Field,
-  FieldDescription,
-  FieldError,
-  FieldGroup,
-  FieldLabel,
-  FieldSeparator,
-  FieldSet,
-  FieldTitle,
-} from '@/components/ui/field';
+import InputField from '@/components/InputField/InputField';
 import { Button } from '@/components/ui/button';
 import Logo from '@/components/Logo/Logo';
 
@@ -16,11 +7,32 @@ export default function SignupPage() {
     <div className="flex flex-col items-center justify-center h-screen bg-neutral-100">
       <div className="flex flex-col items-center justify-center gap-2 bg-white border border-neutral-100 w-[540px] max-w-full p-12 rounded-12">
         <div className="flex flex-col items-center justify-center gap-2">
-          <Logo />
-          <h1 className="text-2xl font-bold">Welcome to Note</h1>
-          <p className="text-sm text-neutral-500">Please log in to continue</p>
+          <div className="mb-4">
+            <Logo />
+          </div>
+
+          <div className="mb-4 text-center flex flex-col items-center gap-2 self-stretch">
+            <h1 className="text-2xl font-bold">Welcome to Note</h1>
+            <p className="text-sm text-neutral-500">
+              Please log in to continue
+            </p>
+          </div>
         </div>
-        <form className="flex flex-col gap-2">
+        <form className="flex flex-col gap-2 w-full pt-6">
+          <InputField
+            label="email"
+            labelName="Email Address"
+            placeholder="email@example.com"
+            type="email"
+            required={true}
+          />
+          <InputField
+            label="password"
+            labelName="Password"
+            placeholder="•••••••"
+            type="email"
+            required={true}
+          />
           <Button variant="default" type="submit">
             Signup
           </Button>
