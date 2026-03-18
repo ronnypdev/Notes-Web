bf17dd5df6112192e989d3dbab805e52c3ed73ff
Ronny Perez <ronnypdev@gmail.com>
2026-03-18 19:33:24 -0400

    Added better API Key

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,11 +1,17 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/db';
+import { dash } from '@better-auth/infra';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
     provider: 'pg',
   }),
+  plugins: [
+    // ... other plugins
+    dash(),
+  ],
   secret: process.env.BETTER_AUTH_SECRET,
+  apiKey: process.env.BETTER_AUTH_API_KEY,
   baseURL: process.env.BETTER_AUTH_URL,
 });
