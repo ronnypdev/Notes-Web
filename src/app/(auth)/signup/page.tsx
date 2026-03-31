124af3f618be307d8adb58ce9f52aec8ef097017
Ronny Perez <ronnypdev@gmail.com>
2026-03-31 15:38:15 -0400

    Implemented sign up schema

--- src/app/(auth)/signup/page.tsx
+++ src/app/(auth)/signup/page.tsx
@@ -1,16 +1,53 @@
 import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { authClient } from '@/lib/auth-client';
 import AuthForm from '../components/AuthForm';
+
+const signUpSchema = z.object({
+  username: z.string().min(1, 'Name is required'),
+  email: z.email('Invalid email address').min(1, 'Email is required'),
+  password: z.string().min(6, 'Password must be at least 6 characters'),
+  callbackurl: z.string().default('/allnotes'),
+});
+
+type signUpSchema = z.infer<typeof signUpSchema>;
 
 export default function Signup() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
-  const { data, error } = await authClient.signUp.email({
-    email,
-    password,
-    name,
-    callbackURL: "/allnotes'",
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<signUpForm>({
+    resolver: zodResolver(signUpSchema),
   });
+
+  async function submitSignUpForm(data: signUpForm) {
+    const { data, error } = await authClient.signUp.email(
+      {
+        email, // user email address
+        password, // user password -> min 8 characters by default
+        name, // user display name
+        image, // User image URL (optional)
+        callbackURL: '/dashboard', // A URL to redirect to after the user verifies their email (optional)
+      },
+      {
+        onRequest: (ctx) => {
+          //show loading
+        },
+        onSuccess: (ctx) => {
+          //redirect to the dashboard or sign in page
+        },
+        onError: (ctx) => {
+          // display the error message
+          alert(ctx.error.message);
+        },
+      },
+    );
+  }
 
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-neutral-100 p-5 md:p-0">
