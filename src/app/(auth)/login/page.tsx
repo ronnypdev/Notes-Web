a3fdccb2eac7033a668f74d904b4f4ac686d66ba
Ronny Perez <ronnypdev@gmail.com>
2026-04-17 17:08:51 -0400

    Implemented login client code but still WIP

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,15 +1,52 @@
 'use client';
-
+import { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { authClient } from '@/lib/auth-client';
 import AuthForm from '../components/AuthForm';
+import { toast } from 'sonner';
 
 export default function Login() {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const router = useRouter();
+
+  const handleLoginForm = (value: LogInFormValues) => {
+    authClient.signIn.email(
+      {
+        email: value.email,
+        password: value.password,
+      },
+      {
+        onRequest: () => {
+          //show loading
+          setIsLoading(true);
+        },
+        onResponse: () => {
+          setIsLoading(false);
+        },
+        onSuccess: () => {
+          //redirect to the dashboard or sign in page
+          toast.success('Congratulation! You successfully logged in', {
+            position: 'top-right',
+          });
+          router.push('/allnotes');
+        },
+        onError: ({ error }) => {
+          toast.error(error.message, {
+            position: 'top-right',
+          });
+        },
+      },
+    );
+  };
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-neutral-100 p-5 md:p-0">
       <AuthForm
         formType="login"
+        onSubmit={handleLoginForm}
+        loading={isLoading}
         formTitle="Welcome to Note"
         formDescription="Please log in to continue"
-        onSubmit={() => {}}
         submitButtonText="Log in"
         loggingWithGoogle={true}
         loggingWithGoogleText="Or log in with:"
