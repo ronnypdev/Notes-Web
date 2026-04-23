7db72ab04b3cd4f79be6e48542925ccc303a968e
Ronny Perez <ronnypdev@gmail.com>
2026-04-23 12:41:02 -0400

    Added props to sendemail component

--- src/lib/emails.ts
+++ src/lib/emails.ts
@@ -3,15 +3,29 @@ import { Resend } from 'resend';
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
-export async function sendEmail() {
+interface SendEmailProps {
+  username: string;
+  fromEmail: string;
+  userEmail: string;
+  emailSubject: string;
+  resetLink: string;
+}
+
+export async function sendEmail({
+  username,
+  fromEmail,
+  userEmail,
+  emailSubject,
+  resetLink,
+}: SendEmailProps) {
   try {
     const { data, error } = await resend.emails.send({
-      from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
-      subject: 'Hello world',
+      from: `${fromEmail} <${fromEmail}>`,
+      to: [userEmail],
+      subject: emailSubject,
       react: ResetPasswordEmail({
-        username: 'John',
-        resetLink: 'https://www.google.com',
+        username: username,
+        resetLink: resetLink,
       }),
     });
 
