ac5e2562029cfb82cea630f7cfb463a57390a1ba
Ronny Perez <ronnypdev@gmail.com>
2026-04-23 14:01:37 -0400

    Implemented client code for forgotpassword and refactored auth email code

--- src/app/(auth)/forgotpassword/page.tsx
+++ src/app/(auth)/forgotpassword/page.tsx
@@ -4,9 +4,8 @@ import { useState } from 'react';
 import { Controller, useForm } from 'react-hook-form';
 
 import { zodResolver } from '@hookform/resolvers/zod';
-import { useRouter } from 'next/navigation';
 import { authClient } from '@/lib/auth-client';
-
+import { forgotPasswordSchema, ForgotPasswordFormValues } from '@/lib/zod';
 import AuthForm from '../components/AuthForm';
 
 import {
@@ -17,31 +16,80 @@ import {
   FieldSet,
 } from '@/components/ui/field';
 import { Input } from '@/components/ui/input';
+import { toast } from 'sonner';
 
 export default function ForgotPassword() {
   const [isLoading, setIsLoading] = useState<boolean>(false);
 
+  const handleForgotPasswordForm = (values: ForgotPasswordFormValues) => {
+    authClient.requestPasswordReset(
+      {
+        email: values.email,
+        redirectTo: '/resetpassword',
+      },
+      {
+        onRequest: () => {
+          setIsLoading(true);
+        },
+        onResponse: () => {
+          setIsLoading(false);
+        },
+        onSuccess: () => {
+          toast.success('Reset password link sent to your email', {
+            position: 'top-right',
+          });
+        },
+        onError: ({ error }) => {
+          toast.error(error.message, {
+            position: 'top-right',
+          });
+        },
+      },
+    );
+  };
+
+  const { control, handleSubmit } = useForm<ForgotPasswordFormValues>({
+    resolver: zodResolver(forgotPasswordSchema),
+    defaultValues: {
+      email: '',
+    },
+  });
+
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-neutral-100 p-5 md:p-0">
       <AuthForm
-        onSubmit={() => {}}
+        onSubmit={handleSubmit(handleForgotPasswordForm)}
         loading={isLoading}
         formTitle="Forgotten your password?"
         formDescription="Enter your email below, and we’ll send you a link to reset it."
         submitButtonText="Send Reset Link">
-        <FieldSet>
-          <FieldGroup>
-            <Field>
-              <FieldLabel htmlFor="forgotpassword">Email Address</FieldLabel>
-              <Input
-                id="forgotpassword"
-                type="email"
-                placeholder="email@example.com"
-                required={true}
-              />
-            </Field>
-          </FieldGroup>
-        </FieldSet>
+        <Controller
+          name="email"
+          control={control}
+          render={({ field, fieldState }) => (
+            <FieldSet>
+              <FieldGroup>
+                <Field>
+                  <FieldLabel htmlFor="forgotpassword">
+                    Email Address
+                  </FieldLabel>
+                  <Input
+                    {...field}
+                    id={field.name}
+                    value={field.value ?? ''}
+                    type="email"
+                    aria-invalid={fieldState.invalid}
+                    placeholder="email@example.com"
+                    required={true}
+                  />
+                  {fieldState.invalid && (
+                    <FieldError errors={[fieldState.error]} />
+                  )}
+                </Field>
+              </FieldGroup>
+            </FieldSet>
+          )}
+        />
       </AuthForm>
     </div>
   );
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import { db } from '@/db';
 import { user, session, account, verification } from '@/db/schema/auth-schema';
+import { sendEmail } from './emails';
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -11,6 +12,15 @@ export const auth = betterAuth({
   emailAndPassword: {
     enabled: true,
     requireEmailVerification: false,
+    sendResetPassword: async ({ user, url, token }) => {
+      void sendEmail({
+        username: user.name,
+        fromEmail: 'noreply@example.com',
+        userEmail: user.email,
+        emailSubject: 'Reset your password',
+        resetLink: `${url}?token=${token}`,
+      });
+    },
   },
 
   secret: process.env.BETTER_AUTH_SECRET,
