664bd5d07b87dda68aed68314c5d9ebb87a473da
Ronny Perez <ronnypdev@gmail.com>
2026-05-01 15:37:18 -0400

    Unit vtest bug WIP

---
+++ src/app/(auth)/components/__test__/AuthForm.test.tsx
@@ -0,0 +1,23 @@
+import { expect, test } from 'vitest';
+import { render } from 'react-dom';
+import AuthForm from '../AuthForm';
+
+test('renders AuthForm component', () => {
+  render(
+    <AuthForm
+      onSubmit={() => {}}
+      loading={false}
+      formTitle="Log in to your account"
+      formDescription="Welcome back! Please enter your details."
+      submitButtonText="Log in"
+      loggingWithGoogle={true}
+      loggingWithGoogleText="Or log in with:"
+      googleButtonText="Google"
+      formFooterText="Don't have an account?"
+      formFooterLink="/signup"
+      formFooterLinkText="Sign up">
+      <></>
+    </AuthForm>,
+  );
+  expect(screen.getByText('Log in to your account')).toBeDefined();
+});
