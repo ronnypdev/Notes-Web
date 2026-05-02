5075ff8cb877978c8fa03548daac4d4146450b66
Ronny Perez <ronnypdev@gmail.com>
2026-05-02 18:01:06 -0400

    Unit testing rebuild pending

--- src/app/(auth)/components/__test__/AuthForm.test.tsx
+++ src/app/(auth)/components/__test__/AuthForm.test.tsx
@@ -1,23 +1,9 @@
-import { expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import AuthForm from '../AuthForm';
 
-test('renders AuthForm component', () => {
-  render(
-    <AuthForm
-      onSubmit={() => {}}
-      loading={false}
-      formTitle="Log in to your account"
-      formDescription="Welcome back! Please enter your details."
-      submitButtonText="Log in"
-      loggingWithGoogle={true}
-      loggingWithGoogleText="Or log in with:"
-      googleButtonText="Google"
-      formFooterText="Don't have an account?"
-      formFooterLink="/signup"
-      formFooterLinkText="Sign up">
-      <></>
-    </AuthForm>,
-  );
-  expect(screen.getByText('Log in to your account')).toBeInTheDocument();
+describe('AuthForm', () => {
+  it('should render the AuthForm component', () => {
+    render(<AuthForm />);
+    expect(screen.getByText('AuthForm')).toBeInTheDocument();
+  });
 });
