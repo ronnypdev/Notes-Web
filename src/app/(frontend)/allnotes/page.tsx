98676296cf40fce00a0fec63c25da5a8899a44e2
Ronny Perez <ronnypdev@gmail.com>
2026-05-11 14:55:19 -0400

    Implemented create button component

--- src/app/(frontend)/allnotes/page.tsx
+++ src/app/(frontend)/allnotes/page.tsx
@@ -1,5 +1,6 @@
 import { getServerSessions } from '@/lib/usersessions';
 import UnauthenticatedPage from '../unauthenticated/page';
+import { Button } from '@/components/ui/button';
 
 export default async function AllNotesPage() {
   const session = await getServerSessions();
@@ -9,8 +10,8 @@ export default async function AllNotesPage() {
 
   return (
     <section className="w-full h-full grid grid-cols-[350px_1fr] grid-rows-[1fr]">
-      <div className="notes-sidebar col-span-1 row-span-1 border-r border-neutral-200 p-4">
-        notes sidebar
+      <div className="notes-sidebar col-span-1 row-span-1 border-r border-neutral-200 py-5 pl-4 pr-8">
+        <Button className="w-full">+ Create New Note</Button>
       </div>
       <div className="notes-content col-span-1 row-span-1 p-4">
         notes content
