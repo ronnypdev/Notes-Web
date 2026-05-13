9a5b26ad03e60557422ea3cea1f4f0f2a750777e
Ronny Perez <ronnypdev@gmail.com>
2026-05-13 16:13:41 -0400

    Properties section WIP

--- src/app/(frontend)/allnotes/[id]/page.tsx
+++ src/app/(frontend)/allnotes/[id]/page.tsx
@@ -1,15 +1,30 @@
 import { Textarea } from '@/components/ui/textarea';
 import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 
 export default function NoteItemDetails() {
   return (
     <>
-      <h4 className="text-neutral-950 text-2xl font-semibold leading-[1.2] tracking-[-0.3px]">
-        Note Title
-      </h4>
-      <Textarea placeholder="Enter your note content here..." />
+      <article>
+        <header>
+          <h6 className="text-neutral-950 font-sans font-bold text-2xl leading-[1.2] tracking-[-0.5px]">
+            React Performance Optimization
+          </h6>
+          <div className="properties">
+            <div className="tags">
+              <span className="tag">tags</span>
+              <Input type="text" placeholder="Add a tag" />
+            </div>
+            <div className="last-modified">
+              <span className="last-modified-label">Last Edited: </span>
+              <span className="last-modified-date">29 Oct 2024</span>
+            </div>
+          </div>
+        </header>
+        <Textarea placeholder="Enter your note content here..." />
 
-      <Button>Save</Button>
+        <Button>Save</Button>
+      </article>
     </>
   );
 }
