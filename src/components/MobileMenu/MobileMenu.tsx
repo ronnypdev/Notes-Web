3ed48f288f49918a6722003a1332af93cd85829c
Ronny Perez <ronnypdev@gmail.com>
2026-05-18 17:29:38 -0400

    Added Menu icon skelleton

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -10,7 +10,7 @@ import {
   SearchIcon,
   ArchiveIcon,
   TagIcon,
-  SettingsIcon,
+  SettingIcon,
 } from '@/components/icons';
 
 export default function MobileMenu() {
@@ -19,8 +19,33 @@ export default function MobileMenu() {
       <NavigationMenu>
         <NavigationMenuList>
           <NavigationMenuItem>
+            <HomeIcon className="size-5" />
             <NavigationMenuLink asChild>
               <Link href="/allnotes">All Notes</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <SearchIcon className="size-5" />
+            <NavigationMenuLink asChild>
+              <Link href="/allnotes">Search</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <ArchiveIcon className="size-5" />
+            <NavigationMenuLink asChild>
+              <Link href="/archivenotes">Archived Notes</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <TagIcon className="size-5" />
+            <NavigationMenuLink asChild>
+              <Link href="/tags">Tags</Link>
+            </NavigationMenuLink>
+          </NavigationMenuItem>
+          <NavigationMenuItem>
+            <SettingIcon className="size-5" />
+            <NavigationMenuLink asChild>
+              <Link href="/settings">Settings</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
         </NavigationMenuList>
