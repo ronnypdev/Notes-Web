e8b7e635f2a76fb9ab7249e3443692d7ceecc971
Ronny Perez <ronnypdev@gmail.com>
2026-05-19 12:07:29 -0400

    fixed mobile menu html injection issue

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -16,33 +16,35 @@ import {
 export default function MobileMenu() {
   return (
     <>
-      <NavigationMenu>
-        <NavigationMenuList>
-          <NavigationMenuItem>
+      <NavigationMenu
+        viewport={false}
+        className="w-full border-t max-w-full bg-white border-neutral-200 shadow-mobile-menu [&>div]:w-full">
+        <NavigationMenuList className="flex items-center justify-between py-[var(--spacing-150)] px-[var(--spacing-200)]">
+          <NavigationMenuItem className="cursor-pointer">
             <HomeIcon className="size-5" />
             <NavigationMenuLink asChild>
               <Link href="/allnotes">All Notes</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem>
+          <NavigationMenuItem className="cursor-pointer">
             <SearchIcon className="size-5" />
             <NavigationMenuLink asChild>
               <Link href="/allnotes">Search</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem>
+          <NavigationMenuItem className="cursor-pointer">
             <ArchiveIcon className="size-5" />
             <NavigationMenuLink asChild>
-              <Link href="/archivenotes">Archived Notes</Link>
+              <Link href="/archivenotes">Archived</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem>
+          <NavigationMenuItem className="cursor-pointer">
             <TagIcon className="size-5" />
             <NavigationMenuLink asChild>
               <Link href="/tags">Tags</Link>
             </NavigationMenuLink>
           </NavigationMenuItem>
-          <NavigationMenuItem>
+          <NavigationMenuItem className="cursor-pointer">
             <SettingIcon className="size-5" />
             <NavigationMenuLink asChild>
               <Link href="/settings">Settings</Link>
