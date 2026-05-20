ee759d12b1e9e8c82be29e9c65f89ac7ecc00d9d
Ronny Perez <ronnypdev@gmail.com>
2026-05-20 12:10:43 -0400

    Restructured mobile nav menu and added hover states for each

--- src/components/MobileMenu/MobileMenu.tsx
+++ src/components/MobileMenu/MobileMenu.tsx
@@ -21,12 +21,14 @@ export default function MobileMenu() {
         viewport={false}
         className="w-full border-t max-w-full bg-white border-neutral-200 shadow-mobile-menu [&>div]:w-full">
         <NavigationMenuList className="flex items-center justify-between py-[var(--spacing-150)] px-[var(--spacing-200)]">
-          <NavigationMenuItem className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600">
-            <HomeIcon className="size-6" />
-            <NavigationMenuLink asChild>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600 hover:text-blue-500 hover:bg-blue-50 group/menu-link"
+              asChild>
               <Link
                 className="font-sans text-sm font-normal leading-[1.2] tracking-[-0.2px]"
                 href="/allnotes">
+                <HomeIcon className="group-hover/menu-link:text-blue-500 size-6" />
                 All Notes
               </Link>
             </NavigationMenuLink>
@@ -35,12 +37,14 @@ export default function MobileMenu() {
             className="data-[orientation=vertical]:h-auto self-stretch shrink-0 hidden md:block lg:hidden"
             orientation="vertical"
           />
-          <NavigationMenuItem className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600">
-            <SearchIcon className="size-6" />
-            <NavigationMenuLink asChild>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600 hover:text-blue-500 hover:bg-blue-50 group/menu-link"
+              asChild>
               <Link
                 className="font-sans text-sm font-normal leading-[1.2] tracking-[-0.2px]"
                 href="#">
+                <SearchIcon className="group-hover/menu-link:text-blue-500 size-6" />
                 Search
               </Link>
             </NavigationMenuLink>
@@ -49,12 +53,14 @@ export default function MobileMenu() {
             className="data-[orientation=vertical]:h-auto self-stretch shrink-0 hidden md:block lg:hidden"
             orientation="vertical"
           />
-          <NavigationMenuItem className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600">
-            <ArchiveIcon className="size-6" />
-            <NavigationMenuLink asChild>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600 hover:text-blue-500 hover:bg-blue-50 group/menu-link"
+              asChild>
               <Link
                 className="font-sans text-sm font-normal leading-[1.2] tracking-[-0.2px]"
                 href="/archivenotes">
+                <ArchiveIcon className="group-hover/menu-link:text-blue-500 size-6" />
                 Archived
               </Link>
             </NavigationMenuLink>
@@ -63,12 +69,14 @@ export default function MobileMenu() {
             className="data-[orientation=vertical]:h-auto self-stretch shrink-0 hidden md:block lg:hidden"
             orientation="vertical"
           />
-          <NavigationMenuItem className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600">
-            <TagIcon className="size-6" />
-            <NavigationMenuLink asChild>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600 hover:text-blue-500 hover:bg-blue-50 group/menu-link"
+              asChild>
               <Link
                 className="font-sans text-sm font-normal leading-[1.2] tracking-[-0.2px]"
                 href="#">
+                <TagIcon className="group-hover/menu-link:text-blue-500 size-6" />
                 Tags
               </Link>
             </NavigationMenuLink>
@@ -77,12 +85,14 @@ export default function MobileMenu() {
             className="data-[orientation=vertical]:h-auto self-stretch shrink-0 hidden md:block lg:hidden"
             orientation="vertical"
           />
-          <NavigationMenuItem className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600">
-            <SettingIcon className="size-6" />
-            <NavigationMenuLink asChild>
+          <NavigationMenuItem>
+            <NavigationMenuLink
+              className="flex flex-col items-center justify-center shrink-0 cursor-pointer py-1 text-neutral-600 hover:text-blue-500 hover:bg-blue-50 group/menu-link"
+              asChild>
               <Link
                 className="font-sans text-sm font-normal leading-[1.2] tracking-[-0.2px]"
                 href="#">
+                <SettingIcon className="group-hover/menu-link:text-blue-500 size-6" />
                 Settings
               </Link>
             </NavigationMenuLink>
