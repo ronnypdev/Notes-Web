abfff8a3efae7e97aac1bae22fadbe4e3220d02f
Ronny Perez <ronnypdev@gmail.com>
2026-05-27 12:57:57 -0400

    implemented  navigation setting structured

--- src/app/(frontend)/settings/layout.tsx
+++ src/app/(frontend)/settings/layout.tsx
@@ -5,11 +5,26 @@ export default function SettingsLayout({
 }: {
   children: React.ReactNode;
 }) {
+  const settingsLinks = [
+    {
+      href: '/settings/change-password',
+      label: 'Change Password',
+    },
+    {
+      href: '/settings/color-theme',
+      label: 'Color Theme',
+    },
+    {
+      href: '/settings/font-options',
+      label: 'Font Options',
+    },
+  ];
+
   return (
     <section className="w-full h-full grid grid-cols-1 lg:grid-cols-[350px_1fr] grid-rows-[1fr]">
       <div className="settings-sidebar w-full col-span-1 row-span-1 border-r border-neutral-200 py-5 pl-4 pr-8">
         <nav>
-          <ul>
+          <ul className="flex flex-col gap-2">
             <li>
               <Link href="/settings/change-password">Change Password</Link>
             </li>
