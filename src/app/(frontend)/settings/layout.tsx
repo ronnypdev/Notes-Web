fa102352c1fd61784f1155d4a17c45f71dcb6386
Ronny Perez <ronnypdev@gmail.com>
2026-05-27 16:38:56 -0400

    Implemented settings navigation bar

--- src/app/(frontend)/settings/layout.tsx
+++ src/app/(frontend)/settings/layout.tsx
@@ -1,22 +1,38 @@
+'use client';
+
+import {
+  SunIcon,
+  LockIcon,
+  TypeIcon,
+  ChevronRightIcon,
+  LogoutIcon,
+} from '@/components/icons';
+import clsx from 'clsx';
+import { usePathname } from 'next/navigation';
 import Link from 'next/link';
+import { Separator } from '@/components/ui/separator';
 
 export default function SettingsLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const pathname = usePathname();
   const settingsLinks = [
-    {
-      href: '/settings/change-password',
-      label: 'Change Password',
-    },
     {
       href: '/settings/color-theme',
       label: 'Color Theme',
+      icon: SunIcon,
     },
     {
       href: '/settings/font-options',
-      label: 'Font Options',
+      label: 'Font Theme',
+      icon: TypeIcon,
+    },
+    {
+      href: '/settings/change-password',
+      label: 'Change Password',
+      icon: LockIcon,
     },
   ];
 
@@ -25,14 +41,38 @@ export default function SettingsLayout({
       <div className="settings-sidebar w-full col-span-1 row-span-1 border-r border-neutral-200 py-5 pl-4 pr-8">
         <nav>
           <ul className="flex flex-col gap-2">
-            <li>
-              <Link href="/settings/change-password">Change Password</Link>
-            </li>
-            <li>
-              <Link href="/settings/color-theme">Color Theme</Link>
-            </li>
-            <li>
-              <Link href="/settings/font-options">Font Options</Link>
+            {settingsLinks.map((link) => {
+              const isActive =
+                pathname === link.href || pathname.startsWith(link.href + '/');
+              return (
+                <li key={link.href}>
+                  <Link
+                    href={link.href}
+                    className={clsx(
+                      'py-1.5 px-3 flex items-center gap-2 text-neutral-950',
+                      {
+                        'rounded-lg bg-neutral-100': isActive,
+                      },
+                    )}>
+                    <link.icon
+                      className={clsx('w-5 h-5', { 'text-blue-500': isActive })}
+                    />
+                    <span>{link.label}</span>
+                    <ChevronRightIcon
+                      className={clsx('w-6 h-6 ml-auto', {
+                        'text-blue-500': isActive,
+                      })}
+                    />
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+          <Separator className="my-4" />
+          <ul className="flex flex-col gap-2">
+            <li className="py-1.5 px-3 flex items-center gap-2 text-neutral-950">
+              <LogoutIcon className="w-5 h-5" />
+              <Link href="/settings/logout">Logout</Link>
             </li>
           </ul>
         </nav>
