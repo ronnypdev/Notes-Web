bde8c22e29fd7ad9547b6055843953abbe9612be
Ronny Perez <ronnypdev@gmail.com>
2026-07-01 10:29:58 -0400

    Implemented empty state for title

--- src/components/NoteItem/NoteItem.tsx
+++ src/components/NoteItem/NoteItem.tsx
@@ -29,9 +29,15 @@ export default function NoteItem({
         id={id}
         className="flex flex-col items-start gap-150 p-100 rounded-(--radius-6)  w-full bg-white hover:bg-neutral-100 transition-colors duration-100 cursor-pointer">
         <ItemHeader>
-          <ItemTitle className="w-full text-neutral-950 font-semibold text-base leading-[1.2] tracking-[-0.3px]">
-            {title}
-          </ItemTitle>
+          {title ? (
+            <ItemTitle className="w-full text-neutral-950 font-semibold text-base leading-[1.2] tracking-[-0.3px]">
+              {title}
+            </ItemTitle>
+          ) : (
+            <ItemTitle className="w-full text-neutral-950 font-semibold text-base leading-[1.2] tracking-[-0.3px]">
+              Enter title
+            </ItemTitle>
+          )}
         </ItemHeader>
         <ItemActions>
           {tags && tags.length > 0 && (
