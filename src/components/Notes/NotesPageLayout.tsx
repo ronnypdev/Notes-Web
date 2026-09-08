6cf6808bf8ca76805f25c4ca87fd2244dc48bf23
Ronny Perez <ronnypdev@gmail.com>
2026-09-08 15:08:58 -0400

    7. NotesPageLayout.tsx

--- src/components/Notes/NotesPageLayout.tsx
+++ src/components/Notes/NotesPageLayout.tsx
@@ -16,7 +16,7 @@ export default function NotesPageLayout({
   return (
     <section className="w-full h-full grid grid-cols-1 lg:grid-cols-[350px_1fr] grid-rows-[1fr]">
       <div
-        className={`notes-sidebar w-full col-span-1 row-span-1 border-r border-neutral-200 py-5 pl-4 pr-8 ${isBaseRoute ? 'block' : 'hidden'} lg:block relative`}>
+        className={`notes-sidebar w-full col-span-1 row-span-1 border-r border-border py-5 pl-4 pr-8 ${isBaseRoute ? 'block' : 'hidden'} lg:block relative`}>
         {notesList}
       </div>
       <div
